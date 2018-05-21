10dc19191f84185f25b46fc70f10bc712653f9d1
Denny Wu <denny.wu87@gmail.com>
2018-05-21 16:38:55 -0700

    Made some edits to ssg js file, but still receiving the same error as before

--- ssg.js
+++ ssg.js
@@ -6,39 +6,43 @@ var fs = require("fs");
 var createHTML = require("create-html")
 var htmlPage = createHTML({
 	title: "Example Page",
-	script: ["example.js", "https://cdn.jsdelivr.net/npm/marked/marked.min.js", "https://code.jquery.com/jquery-3.2.1.js"],
-	scriptAsync: true,
+	script: ["example.js", "https://cdn.jsdelivr.net/npm/marked/marked.min.js"],
+	// scriptAsync: true,
 	lang: "en",
-	head: '<meta name="description" content="example">',
+	// head: '<meta name="description" content="example">',
 	body: '<h1>Title</h1>'
 })
 
 // Creating the server for the html page.
 
-http.createServer(function (req, res) {
-
-  // example.md is the Markdown file that the newly generated html file is supposed to be getting text from.
-
-  fs.readFile('example.md', function(err, data) {
-    if (err) {
-    	console.log(err);
-    };
-
-    console.log(data.toString());
-  });
+var server = http.createServer(function (req, res) {
 
   // index.html is the name of the file created by createHTML.
 
   fs.writeFile('index.html', html, function(err) {
-  	if (err) {
-    	console.log(err);
+    if (err) {
+      console.log(err);
     };
+
+    res.writeHead(200)
+
+    // example.md is the Markdown file that the newly generated html file is supposed to be getting text from.
+
+    fs.readFile('example.md', function(err, data) {
+      if (err) {
+        console.log(err);
+      };
+
+      res.end(data)
+    });
   })
 
-  if (req.url === '/') {
-    res.end(data)
-  };
+});
 
-}).listen(8080);
+  // if (req.url === '/') {
+    
+  // };
 
-console.log("Now listening on local host 8080!")
+server.listen(3000);
+
+console.log("Now listening on http://localhost:3000!")
